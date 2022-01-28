f35cabb894b95a832c0c62fa45501416649ce516
Lucas Leite <lucasvbleite@gmail.com>
2022-01-28 17:45:29 -0300

    calculating volume when Cuboit initializes

--- app/api/model/cuboid.py
+++ app/api/model/cuboid.py
@@ -9,3 +9,10 @@ class Cuboid(db.Model):
     height = db.Column(db.Integer)
     depth = db.Column(db.Integer)
     bag_id = db.Column(db.Integer, db.ForeignKey("bags.id"), nullable=False)
+
+    def __init__(self, width, height, depth, bag_id):
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.bag_id = bag_id
+        self.volume = self.width * self.height * self.depth
